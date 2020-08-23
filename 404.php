96acadb79f74ee1659ca52683022abb7860b4563
Rustam <punk60826@gmail.com>
2020-08-24 01:01:44 +0300

    added 404 page category edu_toys and soft toys also screenshot of site

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php
+    get_header();
+?>
+<div class="container toys">
+    <div class="subtitle"><a href="<?php echo get_home_url();?>">
+     Такой страницы не существует. Вернуться на главную?
+    </a>
+ </div>
+</div>
+<?php 
+    get_footer();
+?>
+
